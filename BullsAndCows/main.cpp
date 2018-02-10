63efbae507b21434924423857c06b33143fc9574
Sidney Harrell <sidney.harrell@gmail.com>
2018-02-10 02:27:39 -0500

    consistency

--- BullsAndCows/main.cpp
+++ BullsAndCows/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 	constexpr int WORD_LENGTH = 5;
 
-	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
 	return 0;
