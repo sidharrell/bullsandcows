c51e7da03b242ae064d44a5415f1fe2abee15c5d
Sidney Harrell <sidney.harrell@gmail.com>
2018-02-10 02:28:38 -0500

    add psudocode

--- BullsAndCows/main.cpp
+++ BullsAndCows/main.cpp
@@ -4,8 +4,13 @@ using namespace std;
 int main() {
 	constexpr int WORD_LENGTH = 5;
 
+	// introduce the game
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
+
+	// get a guess from the player
+
+	// repeat the guess back to them
 	return 0;
 }
